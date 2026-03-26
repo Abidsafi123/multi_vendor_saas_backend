9de94ef8ec3ec8ed4c9b3fef92fd5e495f07c92e
= <=>
2026-03-27 01:02:22 +0500

    server.js updated

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
+import express from "express";
 import dotenv from "dotenv";
-dotenv.config(); // 1️⃣ load env first
-
 import { createApp } from "./src/app.js";
-import connectDb from "./src/config/db.js";
+import connectDb from "./src/config/db.js"
+connectDb()
+const app = createApp()
+ 
+dotenv.config();
 
-const startServer = async () => {
-  try {
-    await connectDb(); // 2️⃣ wait for DB connection
-    console.log("Database connected ✅");
+ 
 
-    const app = createApp(); // 3️⃣ create app after DB
+const port = process.env.PORT || 3000;
 
-    const port = process.env.PORT || 3000;
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port} ✅`);
-    });
-  } catch (err) {
-    console.error("Failed to start server:", err);
-  }
-};
-
-startServer();
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
