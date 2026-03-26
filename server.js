b572d6a4e099f26b9ed99979352875938cef1b2b
= <=>
2026-03-27 00:50:15 +0500

    server.js updated

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
-import { createApp } from "./app.js"; // adjust path if your file is in src/
-import connectDb from "./config/db.js"; // adjust path
-
+import { createApp } from "./src/app.js";
+import connectDb from "./src/config/db.js"
+connectDb()
+const app = createApp()
  
 dotenv.config();
 
- connectDb();
-
- const app = createApp();
-
-// 4️ Start server on dynamic port
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
